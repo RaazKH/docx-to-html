1da48d85aedeaa683c3ff8590709bf02d546ec70
RaazKH <raazkhoshnood@gmail.com>
2023-02-26 15:35:29 +0200

    feat: ignore copyright stuff at bottom of english

--- encoding.py
+++ encoding.py
@@ -1,22 +1,23 @@
-import os
 from docx import Document
 from lxml import etree
+import os
 import random
 
-# Set the input and output directory paths
+# Set the paths for the input and output directories
 input_dir = 'word'
 output_dir = 'html'
 
 # Create the output directory if it doesn't exist
 if not os.path.exists(output_dir):
-    os.mkdir(output_dir)
+    os.makedirs(output_dir)
 
 # Iterate through each file in the input directory
 for filename in os.listdir(input_dir):
     # Check if the file is a Word document
     if filename.endswith('.docx'):
         # Open the Word document
-        doc = Document(os.path.join(input_dir, filename))
+        doc_path = os.path.join(input_dir, filename)
+        doc = Document(doc_path)
 
         # Create an XML element for the HTML document
         html = etree.Element('html')
@@ -25,6 +26,11 @@ for filename in os.listdir(input_dir):
         # Iterate through each paragraph in the document
         for paragraph in doc.paragraphs:
             if paragraph.text.strip():
+                # Check if the paragraph starts with "This document" or "Last Modified"
+                if paragraph.text.strip().startswith('This document') or paragraph.text.strip().startswith('Last Modified'):
+                    # Skip the paragraph
+                    continue
+
                 # Create a new HTML paragraph element and add the text
                 p = etree.Element('p')
 
@@ -39,7 +45,9 @@ for filename in os.listdir(input_dir):
         # Serialize the HTML element to a string
         html_string = etree.tostring(html, encoding='unicode', pretty_print=True)
 
-        # Save the HTML string to a file with the same name as the Word file
-        output_filename = os.path.splitext(filename)[0] + '.html'
-        with open(os.path.join(output_dir, output_filename), 'w') as file:
+        # Create the output file path
+        html_path = os.path.join(output_dir, os.path.splitext(filename)[0] + '.html')
+
+        # Save the HTML string to a file
+        with open(html_path, 'w') as file:
             file.write(html_string)
