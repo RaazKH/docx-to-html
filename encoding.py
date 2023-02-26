3cd6202b00e8b9781a15ee1731a72f24432901a0
RaazKH <raazkhoshnood@gmail.com>
2023-02-26 14:52:50 +0200

    feat: add random a tag at start of every paragraph

--- encoding.py
+++ encoding.py
@@ -1,3 +1,4 @@
+import random
 from docx import Document
 from lxml import etree
 
@@ -6,16 +7,23 @@ doc = Document('test.docx')
 
 # Create an XML element for the HTML document
 html = etree.Element('html')
+head = etree.SubElement(html, 'head')
 body = etree.SubElement(html, 'body')
 
 # Iterate through each paragraph in the document
 previous_was_paragraph = False
 for paragraph in doc.paragraphs:
-    text = paragraph.text.strip()
-    if text:
+    if paragraph.text.strip():
         # Create a new HTML paragraph element and add the text
         p = etree.SubElement(body, 'p')
-        p.text = text
+        
+        # Add a self-closing <a> tag at the start of the paragraph
+        a = etree.Element('a')
+        a.set('class', 'brl-location')
+        a.set('id', str(random.randint(100000000, 999999999)))
+        p.append(a)
+
+        p.text = paragraph.text.strip()
 
 # Serialize the HTML element to a string
 html_string = etree.tostring(html, encoding='unicode', pretty_print=True)
