0f9d10909bab1cefab273f45f6eaa41c217e20ac
RaazKH <raazkhoshnood@gmail.com>
2023-02-26 15:06:11 +0200

    fix: clean up and optimize

--- encoding.py
+++ encoding.py
@@ -1,29 +1,27 @@
-import random
 from docx import Document
 from lxml import etree
+import random
 
 # Open the Word document
 doc = Document('test.docx')
 
 # Create an XML element for the HTML document
 html = etree.Element('html')
-head = etree.SubElement(html, 'head')
 body = etree.SubElement(html, 'body')
 
 # Iterate through each paragraph in the document
-previous_was_paragraph = False
 for paragraph in doc.paragraphs:
     if paragraph.text.strip():
         # Create a new HTML paragraph element and add the text
-        p = etree.SubElement(body, 'p')
-        
+        p = etree.Element('p')
+
         # Add a self-closing <a> tag at the start of the paragraph
-        a = etree.Element('a')
-        a.set('class', 'brl-location')
-        a.set('id', str(random.randint(100000000, 999999999)))
+        a_id = str(random.randint(100000000, 999999999))
+        a = etree.Element('a', {'class': 'brl-location', 'id': a_id})
+        a.tail = paragraph.text.strip()
         p.append(a)
 
-        p.text = paragraph.text.strip()
+        body.append(p)
 
 # Serialize the HTML element to a string
 html_string = etree.tostring(html, encoding='unicode', pretty_print=True)
